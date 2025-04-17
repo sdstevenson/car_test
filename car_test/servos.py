bc61601bdd4ff6439ed850adfee2f8b0145548c4
sdstevenson <sdstevenson@gmail.com>
2025-04-17 15:17:01 -0400

    feat: servo test

---
+++ car_test/servos.py
@@ -0,0 +1,26 @@
+from gpiozero import Servo
+    # Default PWM period of 20 ms matches current servos
+import time
+
+class CustomServo:
+    def __init__(self, pin:int):
+        self.servo:Servo = Servo(pin)
+
+    def set_angle(self, angle:float):
+        """Acceptable inputs from -1 to 1"""
+        # Make sure angle is within acceptable ranges
+        if angle < -1:
+            angle = -1
+        elif angle > 1:
+            angle = 1
+        self.servo.value = angle
+
+def main():
+    servo = CustomServo(pin=18)
+    while True:
+        servo.set_angle(-1)
+        time.sleep(1)
+        servo.set_angle(0)
+        time.sleep(1)
+        servo.set_angle(1)
+        time.sleep(1)
